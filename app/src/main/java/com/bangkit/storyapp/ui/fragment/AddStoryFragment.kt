ddcad6d8b1828311cba40bfd889ea613049eecea
galangriarulhaq <galangrz337@gmail.com>
2024-12-09 09:57:41 +0700

    Add Intent Gallery

--- app/src/main/java/com/bangkit/storyapp/ui/fragment/AddStoryFragment.kt
+++ app/src/main/java/com/bangkit/storyapp/ui/fragment/AddStoryFragment.kt
@@ -1,32 +1,64 @@
 package com.bangkit.storyapp.ui.fragment
 
+import android.net.Uri
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bangkit.storyapp.R
+import com.bangkit.storyapp.databinding.FragmentAddStoryBinding
+import com.bangkit.storyapp.databinding.FragmentHomeBinding
 import com.bangkit.storyapp.ui.model.AddStoryViewModel
 
 class AddStoryFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AddStoryFragment()
-    }
+    private var _binding: FragmentAddStoryBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: AddStoryViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var currentImageUri: Uri? = null
 
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnGallery.setOnClickListener { startGallery() }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_add_story, container, false)
+        _binding = FragmentAddStoryBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    private fun startGallery() {
+        launcherGallery.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private val launcherGallery = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri: Uri? ->
+        if (uri != null) {
+            currentImageUri = uri
+            showImage()
+        } else {
+            Log.d("Photo Picker", "No media selected")
+        }
+    }
+
+    private fun showImage() {
+        currentImageUri?.let {
+            Log.d("Image URI", "showImage: $it")
+            binding.ivPhoto.setImageURI(it)
+        }
+    }
+
 }
