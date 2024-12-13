a4110dbde7eda5ec715efa03c7f53b1ea141ea61
galangriarulhaq <galangrz337@gmail.com>
2024-12-13 09:56:00 +0700

    Create Animation for Register Activity

--- app/src/main/java/com/bangkit/storyapp/ui/RegisterActivity.kt
+++ app/src/main/java/com/bangkit/storyapp/ui/RegisterActivity.kt
@@ -1,5 +1,7 @@
 package com.bangkit.storyapp.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import com.bangkit.storyapp.data.Result
 import android.content.Intent
 import android.os.Bundle
@@ -7,7 +9,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.bangkit.storyapp.R
 import com.bangkit.storyapp.databinding.ActivityRegisterBinding
 import com.bangkit.storyapp.ui.factory.RegisterViewModelFactory
@@ -33,49 +34,18 @@ class RegisterActivity : AppCompatActivity() {
             binding.registerPassword.setText(savedInstanceState.getString("password"))
         }
 
-        setupUI()
-        observeViewModel()
+        updateUI()
 
-        binding.tvSignin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("name", binding.registerName.text.toString())
-        outState.putString("email", binding.registerEmail.text.toString())
-        outState.putString("password", binding.registerPassword.text.toString())
-    }
-
-    private fun setupUI() {
-        binding.btnRegister.setOnClickListener {
-            val name = binding.registerName.text.toString().trim()
-            val email = binding.registerEmail.text.toString().trim()
-            val password = binding.registerPassword.text.toString().trim()
-
-            if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "Fill out All Form", Toast.LENGTH_SHORT).show()
-            } else {
-                registerViewModel.register(name, email, password)
-            }
-        }
-    }
-
-    private fun observeViewModel() {
-
-        registerViewModel.isLoading.observe(this, Observer { isLoading ->
+        registerViewModel.isLoading.observe(this) { isLoading ->
             if (isLoading) {
                 showProgressBar()
             } else {
                 hideProgressBar()
             }
-        })
+        }
 
 
-        registerViewModel.registerResponse.observe(this, Observer { result ->
+        registerViewModel.registerResponse.observe(this) { result ->
             when (result) {
                 is Result.Success -> {
                     hideProgressBar()
@@ -95,8 +65,59 @@ class RegisterActivity : AppCompatActivity() {
                     showProgressBar()
                 }
             }
-        })
+        }
+
+        binding.tvSignin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        playAnimation()
+
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("name", binding.registerName.text.toString())
+        outState.putString("email", binding.registerEmail.text.toString())
+        outState.putString("password", binding.registerPassword.text.toString())
+    }
+
+    private fun updateUI() {
+        binding.btnRegister.setOnClickListener {
+            val name = binding.registerName.text.toString().trim()
+            val email = binding.registerEmail.text.toString().trim()
+            val password = binding.registerPassword.text.toString().trim()
+
+            if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Fill out All Form", Toast.LENGTH_SHORT).show()
+            } else {
+                registerViewModel.register(name, email, password)
+            }
+        }
+    }
+
+    private fun playAnimation() {
+
+        val nameInput = ObjectAnimator.ofFloat(binding.nameInput, View.ALPHA, 1f).setDuration(500)
+        val emailInput = ObjectAnimator.ofFloat(binding.emailInput, View.ALPHA, 1f).setDuration(500)
+        val passwordInput = ObjectAnimator.ofFloat(binding.passInput, View.ALPHA, 1f).setDuration(500)
+        val button = ObjectAnimator.ofFloat(binding.btnFrame, View.ALPHA, 1f).setDuration(500)
+        val info = ObjectAnimator.ofFloat(binding.tvInfo, View.ALPHA, 1f).setDuration(500)
+
+        AnimatorSet().apply {
+            playSequentially(
+                nameInput,
+                emailInput,
+                passwordInput,
+                button,
+                info
+            )
+            startDelay = 100
+        }.start()
+
+    }
+
 
     private fun showProgressBar() {
         binding.btnRegister.text = ""
