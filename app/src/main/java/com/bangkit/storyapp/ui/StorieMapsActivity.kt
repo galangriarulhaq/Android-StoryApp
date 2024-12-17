09fdd6d41c896b0be743896bb841bc91e4b2d03e
galangriarulhaq <galangrz337@gmail.com>
2024-12-17 22:35:30 +0700

    Complete for Stories With Location Features

--- app/src/main/java/com/bangkit/storyapp/ui/StorieMapsActivity.kt
+++ app/src/main/java/com/bangkit/storyapp/ui/StorieMapsActivity.kt
@@ -1,8 +1,16 @@
 package com.bangkit.storyapp.ui
 
+import android.content.res.Resources
+import com.bangkit.storyapp.data.Result
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import com.bangkit.storyapp.R
+import com.bangkit.storyapp.data.local.datastore.UserPreferences
+import com.bangkit.storyapp.data.remote.response.ListStoryItem
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,11 +18,19 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.bangkit.storyapp.databinding.ActivityStorieMapsBinding
+import com.bangkit.storyapp.ui.factory.StoryMapViewModelFactory
+import com.bangkit.storyapp.ui.model.StoryMapViewModel
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MapStyleOptions
 
 class StorieMapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityStorieMapsBinding
+
+    private val storyMapViewModel: StoryMapViewModel by viewModels {
+        StoryMapViewModelFactory.getInstance(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +38,98 @@ class StorieMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityStorieMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val token = UserPreferences(this).getToken()
+        token?.let {
+            storyMapViewModel.getAllStoriesLocation(it)
+        } ?: run {
+            Toast.makeText(this, "Token tidak tersedia", Toast.LENGTH_SHORT).show()
+        }
+
+        storyMapViewModel.storiesLocation.observe(this) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    binding.progressIndicator.visibility = View.VISIBLE
+                }
+
+                is Result.Success -> {
+                    binding.progressIndicator.visibility = View.GONE
+                    val stories = result.data
+                    if (stories.isNotEmpty()) {
+                        addManyMarker(stories)
+                    } else {
+                        Toast.makeText(this, "Tidak ada data cerita", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                is Result.Error -> {
+                    binding.progressIndicator.visibility = View.GONE
+                    Toast.makeText(this, result.error, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isIndoorLevelPickerEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isMapToolbarEnabled = true
+
+        setMapStyle()
     }
+
+    private fun addManyMarker(stories: List<ListStoryItem>) {
+
+        val boundsBuilder = LatLngBounds.Builder()
+
+        stories.forEach { story ->
+            val lat = story.lat
+            val lon = story.lon
+            if (lat != null && lon != null) {
+                val latLng = LatLng(lat as Double, lon as Double)
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(latLng)
+                        .title(story.name)
+                        .snippet(story.description)
+                )
+                boundsBuilder.include(latLng)
+            }
+        }
+
+        val bounds: LatLngBounds = boundsBuilder.build()
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds,
+                resources.displayMetrics.widthPixels,
+                resources.displayMetrics.heightPixels,
+                300
+            )
+        )
+    }
+
+    private fun setMapStyle() {
+        try {
+            val success =
+                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (exception: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", exception)
+        }
+    }
+
+    companion object {
+        private const val TAG = "StoriesMapsActivity"
+    }
+
 }
